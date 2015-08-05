a0daa17bf6e6160af526057f10e492082b14b5fb
nalekkalapudi <naveen.lekkalapudi@getbellhops.com>
2015-08-05 14:59:06 -0400

    adding info

---
+++ CONTRIBUTING.md
@@ -0,0 +1,20 @@
+# Get started
+
+  > sudo pip install requirements.txt
+
+# Start Webserver
+
+## To start webserver:
+   
+   > airflow webserver -p 8080
+
+## To start webserver in debug mode:
+   
+   > airflow webserver -p 8080 -d
+
+# Backfill some day range
+
+  > airflow backfill get_orders_from_postgres_single -s 2015-08-02 -e 2015-08-02
+
+# Run a job
+ 
--- order_assignments.py
+++ order_assignments.py
@@ -4,12 +4,20 @@ from airflow.hooks import PostgresHook, S3Hook
 from datetime import datetime, timedelta
 import csv
 
-task_date = datetime.utcnow() - timedelta(1)
+start_date = datetime.utcnow() - timedelta(days=10)
+end_date = start_date + timedelta(days=20)
 
 default_args = {
     'owner': 'naveen',
-    'depends_on_past': False,
-    'start_date': task_date
+    'depends_on_past': True,
+    'start_date': start_date,
+    'end_date': end_date,
+    'schedule_interval': timedelta(days=0,minutes=5)
+    # 'email': ['naveen.lekkalapudi@getbellhops.com'],
+    # 'email_on_failure': False,
+    # 'retries': 1,
+    # 'retry_interval': timedelta(minustes=5),
+    # 'sla' : timedelta()   # Time by which job is expected to succeed.
 }
 
 dag = DAG('get_orders_from_postgres_single', default_args=default_args)
@@ -17,7 +25,14 @@ dag = DAG('get_orders_from_postgres_single', default_args=default_args)
 def postgres_call(sql):
     pghook = PostgresHook(postgres_conn_id="naveen_ngrok_postgres")
     return pghook.get_records(sql)
-    
+
+def redshift_call(sql, type_='run'):
+    pghook = PostgresHook(postgres_conn_id="naveen_redshift")
+    if type_ == 'run':
+        return pghook.run(sql)
+    else:
+        return pghook.get_records(sql)
+
 def s3_call(file_, bucket_name):
     s3hook = S3Hook(s3_conn_id="naveen_s3")
     s3hook.load_file(
@@ -50,6 +65,23 @@ def store_orders_with_bellhops(**kwargs):
     s3_call(
         file_=file_,
         bucket_name='naveen-airflow')
+    
+def transfer_orders_with_bellhops(**kwargs):
+    drop_table = "drop table order_assignments;"
+    create_table = "create table order_assignments(reserve_date varchar(50), ord_number varchar(50), bellhops varchar(5000));"
+    load_data = "copy order_assignments from 's3://naveen-airflow/latest' credentials 'aws_access_key_id=AKIAJWFVJELCY6SZI7EQ;aws_secret_access_key=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' delimiter ',';"
+    check_table = "select exists (select 1 from information_schema.tables where table_name='order_assignments');" 
+    #sqls = [drop_table, create_table, load_data]
+    table_exists = redshift_call(check_table,'get')[0][0]
+    print "table_exists:", table_exists
+    if table_exists:
+        sqls = [drop_table, create_table, load_data]
+        for i in sqls:
+            redshift_call(i)
+    else:
+        sqls = [create_table, load_data]
+        for i in sqls:
+            redshift_call(i)
 
 postgres_to_local_csv = PythonOperator(
     task_id='postgres_to_local_csv',
@@ -63,6 +95,12 @@ local_csv_to_s3 = PythonOperator(
     python_callable=store_orders_with_bellhops,
     dag=dag)
 
-local_csv_to_s3.set_upstream(postgres_to_local_csv)
+s3_to_redshift = PythonOperator(
+    task_id='s3_to_redshift',
+    provide_context=True,
+    python_callable=transfer_orders_with_bellhops,
+    dag=dag) 
 
+local_csv_to_s3.set_upstream(postgres_to_local_csv)
+s3_to_redshift.set_upstream(local_csv_to_s3)
     
