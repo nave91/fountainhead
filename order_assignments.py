226fa621d1c709b00abf071dcb85daf79f857999
nalekkalapudi <naveen.lekkalapudi@getbellhops.com>
2015-08-05 17:00:20 +0000

    fixing encoding

--- order_assignments.py
+++ order_assignments.py
@@ -22,7 +22,7 @@ def s3_call(file_, bucket_name):
     s3hook = S3Hook(s3_conn_id="naveen_s3")
     s3hook.load_file(
         filename=file_,
-        key=task_date.strftime('%Y-%m-%d'),
+        key=task_date.strftime('latest'),
         bucket_name=bucket_name)
 
 def get_orders_with_bellhops(**kwargs):
@@ -36,9 +36,11 @@ def get_orders_with_bellhops(**kwargs):
     sql_records = postgres_call(sql)
     records = []
     for _r,r in enumerate(sql_records):
-        names = r[2]
-        records.append([r[0].strftime('%Y-%m-%d'),r[1]]+names.split(','))
-
+        try:
+            names = r[2].encode('UTF-8')
+            records.append([r[0].strftime('%Y-%m-%d'),r[1],":".join(names.split(','))])
+        except:
+            continue
     with open("/tmp/records.csv", "wb") as f:
         writer = csv.writer(f)
         writer.writerows(records)
